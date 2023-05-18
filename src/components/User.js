8e0d67b16a1494493a9ab35e820e5e39f889a13a
Vladimir RM <shvdrummer@gmail.com>
2023-05-18 15:05:11 +0300

    Start todo app

--- src/components/User.js
+++ src/components/User.js
@@ -3,6 +3,7 @@ import FirstName from './FirstName'
 import LastName from './LastName'
 import { useDispatch } from 'react-redux'
 import { setFirstName } from '../features/user/userSlice'
+import { setLastName } from '../features/user/userSlice'
 
 const User = () => {
 
@@ -22,6 +23,11 @@ const User = () => {
                 type='text'
                 placeholder='Second Name'
                 className='w-full p-1 mb-2 focus:outline-none focus:border-lime-500 focus: border-2 placeholder:text-sm'
+                onChange={(e) =>{
+                    dispatch(setLastName(e.target.value))
+
+                }}
+            
             />
             <div className='flex gap-20 py-5'>
                 <div className='flex flex-col'>
