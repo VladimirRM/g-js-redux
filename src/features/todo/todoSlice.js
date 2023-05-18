0c8cb88d76e9f537458c4229ded0e8c43e6d3afe
Vladimir RM <shvdrummer@gmail.com>
2023-05-18 15:28:29 +0300

    add todoSlice, in store

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -3,3 +3,15 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
     todos: []
 }
+
+export  const todoSlice = createSlice({
+    name: "todos",
+    initialState,
+      reducers: {
+        addTodo: (state,action) => {
+
+        }
+      }
+})
+export const {addTodo} =  todoSlice.actions
+export default  todoSlice.reducer
