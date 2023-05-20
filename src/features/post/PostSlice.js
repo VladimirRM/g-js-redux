c2e96cc301640415baba1ec10f4b4f7c031cfc22
Vladimir RM <shvdrummer@gmail.com>
2023-05-20 09:46:08 +0300

    import  createAsyncThunk

--- src/features/post/PostSlice.js
+++ src/features/post/PostSlice.js
@@ -1,9 +1,13 @@
-import {createSlice} from '@reduxjs/toolkit'
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
 // https://jsonplaceholder.typicode.com/posts
 
 const initialState = {
     posts: [],
 }
+
+export const getPosts = createAsyncThunk ({
+    
+})
 
 export const postSlice = createSlice({
     name: 'posts',
