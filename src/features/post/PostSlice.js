e98e1ec98fe37ca29cb9651bc172fc1a90560bd5
Vladimir RM <shvdrummer@gmail.com>
2023-05-20 09:52:56 +0300

    import axios

--- src/features/post/PostSlice.js
+++ src/features/post/PostSlice.js
@@ -1,13 +1,17 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import axios from "axios"
 // https://jsonplaceholder.typicode.com/posts
 
 const initialState = {
     posts: [],
 }
 
-export const getPosts = createAsyncThunk ({
-    
-})
+export const getPosts = createAsyncThunk (
+    "posts/getPosts",async (_,{rejectWithValue})=>{
+       const result = await axios
+    }
+
+)
 
 export const postSlice = createSlice({
     name: 'posts',
