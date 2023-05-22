dfc4698840f1dba90c9a9707069d6d0737732be0
Vladimir RM <shvdrummer@gmail.com>
2023-05-22 11:49:02 +0300

    Start create reducers.

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,2 +1,14 @@
 import  {createSlice} from "@reduxjs/toolkit"
 
+const initialState = {
+    firstname: "",
+    lastname:"",
+}
+
+export const userSlice = createSlice({
+    name:" user",
+    initialState,
+    reducers:{
+        
+    }
+})
