83e8b4eb17a9524d53fa09e6ec173508b540cdd5
Vladimir RM <shvdrummer@gmail.com>
2023-05-23 10:41:07 +0300

    Rewrite code input

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,7 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userSlice  from "../features/user/userSlice";
+import{configureStore}  from "@reduxjs/toolkit"
 
 export const store = configureStore({
     reducer:{
-      user: userSlice 
+
     }
 })
-
