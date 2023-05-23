105e3737104cec7a0595f0df57b75f50a20b52d5
Vladimir RM <shvdrummer@gmail.com>
2023-05-23 11:33:46 +0300

    Create slice firstName

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,23 +1,16 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import   {createSlice} from "@reduxjs/toolkit"
 
 const initialState = {
-     firstName: "",
-     lastName: ""
+    firstName: '',
+    lastName: " ",
 }
 
- export const userSlice = createSlice({
-    name:"user",
+export const userSlice = createSlice({
+    name:'user',
     initialState,
-    reducers: {
-        setFirstName: (state,action)=>{
+    reducers:{
+        serFirstName:(state,action)=>{
             state.firstName = action.payload
-        },
-        setLastName: (state,action)=>{
-            state.lastName = action.payload
         }
     }
- })
-
- export const {setFirstName,setLastName} =  userSlice.actions
- export default  userSlice.reducer 
+})
