d41a9124f32d424b3ab5ba78da7296a28a664a7b
Vladimir RM <shvdrummer@gmail.com>
2023-05-23 16:17:29 +0300

    Add onChange for fisrtName input

--- src/components/User.js
+++ src/components/User.js
@@ -5,13 +5,15 @@ import { setFirstName,setLastName } from '../features/user/userSlice'
 import { useDispatch } from 'react-redux'
 
 const User = () => {
+    const dispatch = useDispatch()
     return (
         <div className='flex flex-col'>
             <input
                 type='text'
                 placeholder='First Name'
                 className='w-full p-1 mb-2 focus:outline-none focus:border-lime-500 focus: border-2 placeholder:text-sm'
-            />
+            
+            onChange={(e)=>dispatch(setFirstName(e.target.value))}/>
             <input
                 type='text'
                 placeholder='Second Name'
