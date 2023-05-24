1faf9ff53fbc7c9baf6e47061ae9e7611148a0a8
Vladimir RM <shvdrummer@gmail.com>
2023-05-24 13:58:15 +0300

    Change todo place

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@
 import './App.css';
 import User from './components/User';
 import Form from './components/Form';
-
-
-
-
-
-
-
+import TodoItem from './components/TodoItem';
+import{useSelector} from  'react-redux'
 
 
 function App() {
 
+const todos = useSelector((state)=>state.todos.todo)
   return (
     <div className="App">
 
    <User/>
    <Form/>
-  
+  { todos?.map((todo)=>(
+   <TodoItem key={todo.id}
+    todo={todo}/>
+
+  ))}
 
     </div>
   );
