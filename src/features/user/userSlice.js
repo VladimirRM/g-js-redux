16386b66aecd86f302b8cbd20a245341402f4cad
Vladimir RM <shvdrummer@gmail.com>
2023-05-25 09:14:58 +0300

    Create userSlice

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -12,7 +12,15 @@ const initialState = {
 export const userSlice = createSlice({
     name: 'user',
     initialState,
-    reduser:{
-        
+    reducer:{
+           setFirstName: (state,action)=>{
+            state.firstName = action.payload
+           },
+           setLastName: (state,action)=>{
+            state.lastName = action.payload
+           }
     }
 })
+
+export const  {setFirstName,setLastName} = createSlice.actions
+export default userSlice.reducer
