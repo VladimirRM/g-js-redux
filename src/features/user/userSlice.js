0d3772483516cc6b188b7c42e81aae07e2ab7919
Vladimir RM <shvdrummer@gmail.com>
2023-05-25 09:24:06 +0300

    Add code comp FIrstName

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -22,5 +22,5 @@ export const userSlice = createSlice({
     }
 })
 
-export const  {setFirstName,setLastName} = createSlice.actions
+export const  {setFirstName,setLastName} = userSlice.actions
 export default userSlice.reducer
