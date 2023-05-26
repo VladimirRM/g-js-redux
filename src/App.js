d299dd5ad3363241429474894c698ce87f9629c2
Vladimir RM <shvdrummer@gmail.com>
2023-05-26 09:48:24 +0300

    Add map for render TodoItem

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 
 import './App.css';
 import User from './components/User';
-
+import { useSelector } from 'react-redux';
+import Form from './components/Form';
+import TodoItem from "./components/TodoItem"
 
 
 
 
 function App() {
 
+  const todos = useSelector()
+
 
   return (
     <div className="App">
 
    <User/>
- 
+   <Form/>
 
+      {todos?.map((todo)=>(
+   <TodoItem/>
+
+      ))}
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import {useDispatch}  from "react-redux"
 import { addTodo } from "../features/todo/todoSlice"
-import  {v4} from uuid
+import  {v4} from "uuid"
 
 const Form = () => {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch((state)=>state.todos.todo)
 
     const [todoValue,setTodoValue] = React.useState('')
 
@@ -19,6 +19,7 @@ const Form = () => {
     return (
         <form className='w-full flex' onSubmit={(e) => e.preventDefault()}>
             <input
+            value={todoValue}
                 type='text'
                 placeholder='Type something...'
                 className='w-full p-1 focus:outline-none focus:border-lime-500 focus: border-2 placeholder:text-sm'
@@ -27,6 +28,7 @@ const Form = () => {
             <button
                 type='submit'
                 className='shrink-0 bg-lime-300  hover:bg-lime-400 transition-all px-3 text-sm'
+              onClick={()=>addTodoHandler()}
             >
                 Submit
             </button>
