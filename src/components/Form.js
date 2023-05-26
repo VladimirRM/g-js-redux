5481c0edf37f5987a01ceb76036136b2f2adf8ce
Vladimir RM <shvdrummer@gmail.com>
2023-05-26 09:26:42 +0300

    Add onChange for input

--- src/components/Form.js
+++ src/components/Form.js
@@ -22,7 +22,8 @@ const Form = () => {
                 type='text'
                 placeholder='Type something...'
                 className='w-full p-1 focus:outline-none focus:border-lime-500 focus: border-2 placeholder:text-sm'
-               />
+               
+               onChange={(e)=>setTodoValue(e.target.value)}/>
             <button
                 type='submit'
                 className='shrink-0 bg-lime-300  hover:bg-lime-400 transition-all px-3 text-sm'
