353d9244190937cee8295b00780b91d73725c8cc
Vladimir RM <shvdrummer@gmail.com>
2023-05-31 16:27:21 +0300

    Create firstName reducer

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,1 +1,19 @@
-import {}
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+const initialState = {
+    firstName: "",
+    lastName: "",
+}
+
+
+const userSlice = createSlice({
+          name: "user",
+          initialState,
+          reducers:{
+            setFirstName:(state,action)=>{
+                state.firstName = action.payload
+            }
+          }
+})
