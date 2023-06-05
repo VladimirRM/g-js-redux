d90f695313a10a2c5be375d180cee4c22e3966d9
Vladimir RM <shvdrummer@gmail.com>
2023-06-05 12:33:24 +0300

    Create first reducer

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -5,3 +5,13 @@ const initialState = {
     firstName: '',
     lastName: "",
 }
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers:{
+        setFirstName:(state,actions)=>{
+            state.firstName = action.payload
+        }
+    }
+})
