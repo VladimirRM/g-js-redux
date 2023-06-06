313b500be4003cc5dbf7f02f2d63ff807294285b
Vladimir RM <shvdrummer@gmail.com>
2023-06-06 09:18:48 +0300

    Change code all component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
 import './App.css';
 import User from './components/User';
-import Form from './components/Form';
-import TodoItem from './components/TodoItem';
-import { useSelector } from 'react-redux';
+
+
+
 
 
 
@@ -25,11 +25,7 @@ function App() {
   return (
     <div className="App"> 
     <Form/>
-    {todos?.map((todo)=>(
-    <TodoItem
-    key={todo.id}
-    todo={todo}/>
-     ))}
+ 
  
 <User/>
  
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import  userSlice  from "../features/user/userSlice";
-import todoSlice from "../features/todo/todoSlice";
+
 
 
 
@@ -8,6 +8,6 @@ import todoSlice from "../features/todo/todoSlice";
 export const store = configureStore({
     reducer:{
         user: userSlice,
-        todo: todoSlice,
+
     }
 })
