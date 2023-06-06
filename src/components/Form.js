400ecbf002ef87f8b75254922828ca468ed5fcfc
Vladimir RM <shvdrummer@gmail.com>
2023-06-06 09:27:49 +0300

    Import dispatch in form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 
 
 const Form = () => {
+    const dispatch = useDispatch()
     return (
         <form className='w-full flex' onSubmit={(e) => e.preventDefault()}>
             <input
