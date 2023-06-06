4e312a5e10deb98315d68ea3e09be24aab338b9b
Vladimir RM <shvdrummer@gmail.com>
2023-06-06 09:25:21 +0300

    Import todoSlice in store

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import  userSlice  from "../features/user/userSlice";
+import todoSlice from '../features/todo/todoSlice'
 
 
 
@@ -8,6 +9,7 @@ import  userSlice  from "../features/user/userSlice";
 export const store = configureStore({
     reducer:{
         user: userSlice,
+        todo: todoSlice,
 
     }
 })
