c7e2a7e0e9e0ef808f97e6a7f6909c8ab2b4bac0
Vladimir RM <shvdrummer@gmail.com>
2023-06-07 07:36:24 +0300

    Render PostItem with map

--- src/features/post/Posts.js
+++ src/features/post/Posts.js
@@ -1,20 +1,29 @@
 import React from 'react'
 import PostItem from './PostItem'
-import { useDispatch } from 'react-redux'
+import { useDispatch,useSelector } from 'react-redux'
 import { getPosts } from './PostSlice'
 
 const Posts = () => {
 
     const dispatch = useDispatch()
+
+    const posts = useSelector((state)=>state.post.posts)
     return (
         <div>
             <button
                 type='submit'
                 className='bg-lime-300  hover:bg-lime-400 transition-all p-2 text-sm'
+            onClick={()=>dispatch(getPosts())}
             >
                 Get posts
             </button>
-            <PostItem />
+            {posts?.map((post)=>(
+            
+            <PostItem 
+            key={post.title}
+            />
+
+            ))}
         </div>
     )
 }
