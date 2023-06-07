08cd75486d84b644e0344e7760616e9f2b9d81c9
Vladimir RM <shvdrummer@gmail.com>
2023-06-07 10:07:12 +0300

    Delete code posts

--- src/features/post/PostItem.js
+++ src/features/post/PostItem.js
@@ -6,7 +6,7 @@ const PostItem = ({post}) => {
     const dispatch = useDispatch()
     return (
         <div className='flex w-full bg-indigo-500 hover:bg-indigo-300 transition-all py-1 px-2 text-white rounded-sm cursor-pointer mt-4'
-          onClick
+          onClick={()=>dispatch(deletePostById(post.id))}
         >
             {post.title}
         </div>
