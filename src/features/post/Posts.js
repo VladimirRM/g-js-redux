a2232dc8e215e8c47ccbae0f40891a2dd521cfa3
Vladimir RM <shvdrummer@gmail.com>
2023-06-09 11:18:57 +0300

    Cretate initialState

--- src/features/post/Posts.js
+++ src/features/post/Posts.js
@@ -1,28 +1,16 @@
 import React from 'react'
 import PostItem from './PostItem'
-import { getPosts } from './PostSlice'
-import { useDispatch,useSelector } from 'react-redux'
 
 const Posts = () => {
-    const dispatch = useDispatch()
-
-    const posts = useSelector((state)=>state.post.posts)
     return (
         <div>
             <button
                 type='submit'
                 className='bg-lime-300  hover:bg-lime-400 transition-all p-2 text-sm'
-                onClick={()=>dispatch(getPosts())}
             >
                 Get posts
             </button>
-            {posts?.map((post)=>(
-            <PostItem 
-               key={post.title}
-               post={post}
-            />
-
-            ))}
+            <PostItem />
         </div>
     )
 }
