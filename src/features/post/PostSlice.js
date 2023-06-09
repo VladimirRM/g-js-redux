47b2ad96f247f1d1b67b81565da99a12159fd353
Vladimir RM <shvdrummer@gmail.com>
2023-06-09 11:20:09 +0300

    Create firstSlice

--- src/features/post/PostSlice.js
+++ src/features/post/PostSlice.js
@@ -8,4 +8,9 @@ const initialState = {
 export const postSlice = createSlice({
     name:"posts",
     initialState,
+    reducers:{
+        setPosts: (state,action)=>{
+            state.posts = action.payload
+        }
+    }
 })
