ad75680f1ad8934ff4462f122433ad2f550d3205
Vladimir RM <shvdrummer@gmail.com>
2023-06-09 09:54:34 +0300

    Add getPOsts in Post component

--- src/features/post/Posts.js
+++ src/features/post/Posts.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import PostItem from './PostItem'
+import { getPosts } from './PostSlice'
+import { useDispatch,useSelector } from 'react-redux'
 
 const Posts = () => {
+    const dispatch = useDispatch()
+
+    const posts = useSelector((state)=>state.post.posts)
     return (
         <div>
             <button
                 type='submit'
                 className='bg-lime-300  hover:bg-lime-400 transition-all p-2 text-sm'
+            
             >
                 Get posts
             </button>
