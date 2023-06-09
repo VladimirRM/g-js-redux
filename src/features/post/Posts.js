ce14d96fadd95147f5ab70d7fdbcaf52392ba75a
Vladimir RM <shvdrummer@gmail.com>
2023-06-09 09:56:49 +0300

    Create render use map.

--- src/features/post/Posts.js
+++ src/features/post/Posts.js
@@ -12,11 +12,17 @@ const Posts = () => {
             <button
                 type='submit'
                 className='bg-lime-300  hover:bg-lime-400 transition-all p-2 text-sm'
-            
+                onClick={()=>dispatch(getPosts())}
             >
                 Get posts
             </button>
-            <PostItem />
+            {posts?.map((post)=>(
+            <PostItem 
+               key={post.title}
+               post={post}
+            />
+
+            ))}
         </div>
     )
 }
