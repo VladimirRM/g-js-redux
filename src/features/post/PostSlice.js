5dd03ba38d9996d24eeed1f27f73d3456c1e3522
Vladimir RM <shvdrummer@gmail.com>
2023-06-10 13:23:19 +0300

    Start new version

--- src/features/post/PostSlice.js
+++ src/features/post/PostSlice.js
@@ -1,50 +1,7 @@
-import  { createSlice,createAsyncThunk}  from "@reduxjs/toolkit"
-import axios from "axios"
+// https://jsonplaceholder.typicode.com/posts
+
+import {createSlice}  from  '@reduxjs/toolkit'
 
 
 
-const initialState = { 
-     posts:[],
-}
-
-export const deletePostById = createAsyncThunk(
-    'post.deletePostById', async (id,{rejectWithValue,dispatch})=>{
-          await axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
-          dispatch(deletePost(id))
-    }
-)
-
-export const getPosts = createAsyncThunk(
-    'posts/getPosts', async(_,{rejectWithValue,dispatch})=>{
-           const res =  await axios.get('https://jsonplaceholder.typicode.com/posts')
-            dispatch(setPosts(res.data))
-
-    }
-)
-
-export const postSlice = createSlice({
-    name:'posts',
-    initialState,
-      reducers:{
-        setPosts:(state,action)=>{
-            state.posts = action.payload
-        }, 
-        deletePost:(state,action)=>{
-            state.posts = state.posts.filter((post)=> post.id !== action.payload)
-        }
-      },
-      extraReducers:{
-        [getPosts.fulfilled]:()=>console.log("fulfilled"),
-        [getPosts.pending]:()=>console.log("pending"),
-        [getPosts.rejected]:()=>console.log("rejected"),
-        [deletePostById.fulfilled]:()=>console.log("fulfilled"),
-        [deletePostById.pending]:()=>console.log("pending"),
-        [deletePostById.rejected]:()=>console.log("rejected"),
-      }
-
-})
-
-export const {setPosts,deletePost}  = postSlice.actions
-export default postSlice.reducer
-
-
+const 
--- src/features/post/Posts.js
+++ src/features/post/Posts.js
@@ -1,30 +1,16 @@
 import React from 'react'
 import PostItem from './PostItem'
-import {useDispatch,useSelector}  from 'react-redux'
-import { getPosts } from './PostSlice'
-
 
 const Posts = () => {
-
-    const dispatch = useDispatch()
-
-    const posts = useSelector((state)=> state.post.posts)
     return (
         <div>
             <button
                 type='submit'
                 className='bg-lime-300  hover:bg-lime-400 transition-all p-2 text-sm'
-                onClick={()=> dispatch(getPosts())}
             >
                 Get posts
             </button>
-            {posts?.map((post)=> (
-            <PostItem 
-            key={post.title}
-            post = {post}
-            />
-
-            ))}
+            <PostItem />
         </div>
     )
 }
