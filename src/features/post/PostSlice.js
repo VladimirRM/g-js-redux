c2d53940c2e8936aa5715021f8d04ad5a446fa03
Vladimir RM <shvdrummer@gmail.com>
2023-06-11 10:48:34 +0300

    Add link for get req

--- src/features/post/PostSlice.js
+++ src/features/post/PostSlice.js
@@ -7,7 +7,7 @@ const initialState = {
 
 export const getPosts = createAsyncThunk(
     'posts/getPosts', async(_,{rejectWithValue,dispatch})=>{
-        const res = await axios.get('')
+        const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
     }
 )
 
