928859489a03d6eb68a2f83788db862ef5ca9f93
Vladimir RM <shvdrummer@gmail.com>
2023-06-12 13:08:30 +0300

    Create const getPosts

--- src/features/post/PostSlice.js
+++ src/features/post/PostSlice.js
@@ -7,7 +7,8 @@ const initialState = {
 }
 export const getPosts = createAsyncThunk(
     'post/getPosts',async (_,{rejectWithValue,dispatch})=>{
-        const res = await axios.get('')
+        const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        dispatch(setPosts(res.data))
     }
 )
 
