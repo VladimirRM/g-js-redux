9a9809933b8264e37a0c08969df326eb3ec66234
Vladimir RM <shvdrummer@gmail.com>
2023-06-13 12:07:40 +0300

    Import deletePOstByID

--- src/features/post/PostItem.js
+++ src/features/post/PostItem.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 
 const PostItem = ({post}) => {
+
+    const dispatch = useDispatch()
     return (
         <div className='flex w-full bg-indigo-500 hover:bg-indigo-300 transition-all py-1 px-2 text-white rounded-sm cursor-pointer mt-4'>
             {post.title}
