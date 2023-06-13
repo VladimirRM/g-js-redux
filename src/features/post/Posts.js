f47aa2d775daef3b8abe8cb366b41a2f0889ba28
Vladimir RM <shvdrummer@gmail.com>
2023-06-13 11:54:23 +0300

    Import GetPosts

--- src/features/post/Posts.js
+++ src/features/post/Posts.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PostItem from './PostItem'
 import { useSelector,useDispatch } from 'react-redux'
+import {getPosts}  from './PostSlice'
 
 const Posts = () => {
 
@@ -12,6 +13,7 @@ const Posts = () => {
             <button
                 type='submit'
                 className='bg-lime-300  hover:bg-lime-400 transition-all p-2 text-sm'
+                onClick={()=>dispatch(getPosts())}
             >
                 Get posts
             </button>
