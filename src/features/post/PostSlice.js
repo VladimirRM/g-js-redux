0c53d1df6b69a19196ebdc4e7e09332ed663970b
Vladimir RM <shvdrummer@gmail.com>
2023-06-14 11:23:33 +0300

    Add action deletePost

--- src/features/post/PostSlice.js
+++ src/features/post/PostSlice.js
@@ -1,16 +1,24 @@
 // https://jsonplaceholder.typicode.com/posts
 
 import {createSlice,createAsyncThunk}   from "@reduxjs/toolkit"
+import axios from "axios"
 
 
 const initialState = {
     posts: [],
 }
 
+export const deletedPostById = createAsyncThunk(
+    "posts/deletePostById",async (id,{rejectWithValue,dispatch})=>{
+          await axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
+          dispatch()
+    }
+)
+
 export const getPosts = createAsyncThunk(
     'posts/getPosts',async (_,{rejectWithValue,dispatch}) => {
           const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
-          dispatch(setPosts(set.data))
+          dispatch(setPosts(res.data))
     }
 )
 
@@ -23,6 +31,9 @@ export const postSlice = createSlice({
         setPosts:(state,action)=>
             {state.posts = action.payload
             },
+            deletePost:(state,action)=> {
+                state.posts = state.posts.filter((post)=> post.id !==action.payload)
+            }
         },
 })
 
