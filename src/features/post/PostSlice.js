4aaf153aac5b0478a249867fe13d18d24e9d52ce
Vladimir RM <shvdrummer@gmail.com>
2023-06-14 11:07:58 +0300

    Add action setPosts

--- src/features/post/PostSlice.js
+++ src/features/post/PostSlice.js
@@ -4,9 +4,17 @@ import {createSlice,createAsyncThunk}   from "@reduxjs/toolkit"
 
 
 const initialState = {
-    posts: [,]
+    posts: [],
 }
 
 
 
-export const postSlice = createSlice()
+export const postSlice = createSlice({
+    name:'posts',
+    initialState,
+    reducers:{
+        setPosts:(state,action)=>
+            {state.posts = action.payload
+            },
+        },
+})
