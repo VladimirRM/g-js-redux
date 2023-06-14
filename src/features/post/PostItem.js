97c76760313dc2464db338616de2abaf98dd8e0c
Vladimir RM <shvdrummer@gmail.com>
2023-06-14 11:28:04 +0300

    Create comp PostItem

--- src/features/post/PostItem.js
+++ src/features/post/PostItem.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { deletedPostById } from './PostSlice'
 
-const PostItem = () => {
+const PostItem = ({post}) => {
+
+    const dispatch = useDispatch()
     return (
-        <div className='flex w-full bg-indigo-500 hover:bg-indigo-300 transition-all py-1 px-2 text-white rounded-sm cursor-pointer mt-4'>
-            Post title
+        <div className='flex w-full bg-indigo-500 hover:bg-indigo-300 transition-all py-1 px-2 text-white rounded-sm cursor-pointer mt-4'
+        onClick={()=> dispatch(deletedPostById(post.id))}
+        >
+            {post.title}
         </div>
     )
 }
