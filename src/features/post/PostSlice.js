d3e9b73394215686e6b8432cb22e8edadb88fe95
Vladimir RM <shvdrummer@gmail.com>
2023-06-19 09:59:09 +0300

    create Slice PostsSlice

--- src/features/post/PostSlice.js
+++ src/features/post/PostSlice.js
@@ -6,5 +6,11 @@ const initialState = {
 }
 
 export const postSlice =   createSlice({
-
+     name: 'posts',
+     initialState,
+     reducers:{
+        setPosts:(state,action)=>{
+            state.posts = action.payload
+        }
+     }
 })
