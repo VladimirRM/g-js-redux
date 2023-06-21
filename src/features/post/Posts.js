d29d4250137afc3097fda2ae516c1c95b31cf75b
Vladimir RM <shvdrummer@gmail.com>
2023-06-21 09:07:04 +0300

    Create const posts

--- src/features/post/Posts.js
+++ src/features/post/Posts.js
@@ -4,15 +4,18 @@ import { useDispatch,useSelector } from 'react-redux'
 
 const Posts = () => {
 
-    const dispatch = useDispatch()
-
-
     
+    const dispatch = useDispatch()
+    const posts = useSelector((state)=> state.post.posts)
+
+
+
     return (
         <div>
             <button
                 type='submit'
                 className='bg-lime-300  hover:bg-lime-400 transition-all p-2 text-sm'
+            
             >
                 Get posts
             </button>
