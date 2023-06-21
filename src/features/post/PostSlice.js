03211a9f4960426525644959810326c6d93420c5
Vladimir RM <shvdrummer@gmail.com>
2023-06-21 08:53:34 +0300

    Add initialState

---
+++ src/features/post/PostSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialstate = {
+    posts:[],
+
+}
+
+
+export const postSlice = ({
+    name:'posts',
+    initialState,
+    reducers:{
+        
+    }
+})
