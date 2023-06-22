0b147e563b6fc6f322ad84fbbbd11ae8dd6fec95
Vladimir RM <shvdrummer@gmail.com>
2023-06-22 09:17:15 +0300

    Delete PostSlice

--- src/features/post/Posts.js
+++ src/features/post/Posts.js
@@ -1,34 +1,29 @@
 import React from 'react'
 import PostItem from './PostItem'
-import { useDispatch,useSelector } from 'react-redux'
-import { getPosts } from './PostSlice'
+import {useDispatch,useSelector}  from 'react-redux'
+import {getPosts}  from './PostSlice'
 
 const Posts = () => {
 
-    
+
     const dispatch = useDispatch()
+
     const posts = useSelector((state)=> state.post.posts)
-
-
-
     return (
         <div>
             <button
                 type='submit'
                 className='bg-lime-300  hover:bg-lime-400 transition-all p-2 text-sm'
-              onClick={()=> dispatch(getPosts())} 
-            >
+              onClick={()=> dispatch(getPosts())}
+              >
                 Get posts
             </button>
-            {posts?.map((post)=>{
-            <PostItem
-            post={post}
-            key={post.title
-            }
-            
-            />
+            {posts?.map((post)=>(
+            <PostItem 
+              post={post}
+            key={post.title}/>
 
-            })}
+            ))}
         </div>
     )
 }
