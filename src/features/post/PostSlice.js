6cb41bc34525074481f2df7e09c18146078b7048
Vladimir RM <shvdrummer@gmail.com>
2023-06-23 08:09:00 +0300

    delete code Posts

--- src/features/post/PostSlice.js
+++ src/features/post/PostSlice.js
@@ -1,5 +1,5 @@
-import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios"
 
 
 
@@ -8,31 +8,32 @@ const initialState = {
 }
 
 export const deletePostById = createAsyncThunk(
-    'posts?deletePostById' ,async (id,{rejectWithValue,dispatch})=>{
-        await axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
-        dispatch(deletePost(id))
+    'posts/deletePostById', async (id,{rejectWithValue,dispatch})=>{
+      await axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      dispatch(deletePost(id))
     }
 )
 
-
 export const getPosts = createAsyncThunk(
-       'posts/getPosts', async (_,{rejectWithValue,dispatch})=>{
+    'posts/getPosts', async (_,{rejectWithValue,dispatch})=>{
       const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
       dispatch(setPosts(res.data))
-       }
+    }
 )
 
 export const postSlice = createSlice({
-    name: 'posts',
+    name:'posts',
     initialState,
     reducers:{
         setPosts:(state,action)=>{
-            state.posts = action.payload
+            state.posts= action.payload
         },
         deletePost:(state,action)=>{
-            state.posts = state.posts.filter((post)=>post.id !==action.payload)
+            state.posts = state.posts.filter((post)=> post.id !==action.payload)
         }
     }
 })
-export const {setPosts,deletePost}  = postSlice.actions
+
+
+export const {setPosts,deletePost} = postSlice.actions
 export default postSlice.reducer
