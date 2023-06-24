dee4f4246eb0b5b6afeb3d29707f7059a4953ce7
Vladimir RM <shvdrummer@gmail.com>
2023-06-24 10:51:54 +0300

    Export reducer and action form postSlice

--- src/features/post/PostSlice.js
+++ src/features/post/PostSlice.js
@@ -13,6 +13,9 @@ export const postSlice = createSlice({
         setPosts:(state,action)=>{
             state.posts = action.payload
         },
-        
+
     }
 })
+
+export const {setPosts} = postSlice.actions
+export default postSlice.reducer
