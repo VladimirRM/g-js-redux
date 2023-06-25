575b2a1d79805ebef04f39d355a1d2c86a6887da
Vladimir RM <shvdrummer@gmail.com>
2023-06-25 09:38:49 +0300

    Export acrions and reducer

--- src/features/post/PostSlice.js
+++ src/features/post/PostSlice.js
@@ -9,6 +9,12 @@ export const postSlice = createSlice({
     name: 'posts',
     initialState,
     reducers:{
-        
+       setPosts:(state,action)=>{
+        state.posts = action.payload
+       } 
     }
 })
+
+
+export const {setPosts}  = postSlice.actions
+export default postSlice.reducer
