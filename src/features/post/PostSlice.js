927803adef924a9e502d31ae2fffdf1ff129e65f
Vladimir RM <shvdrummer@gmail.com>
2023-06-25 09:36:56 +0300

    Add reducer setPosts

--- src/features/post/PostSlice.js
+++ src/features/post/PostSlice.js
@@ -1,3 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 
+const initialState = {
+    posts: [],
+}
+
+export const postSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducers:{
+        
+    }
+})
