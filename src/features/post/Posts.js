b952c4ff07a0b9f1dfc3dc469b87532598d111e5
Vladimir RM <shvdrummer@gmail.com>
2023-06-26 15:07:17 +0300

    Add dispatch amd render use map

--- src/features/post/Posts.js
+++ src/features/post/Posts.js
@@ -3,15 +3,26 @@ import PostItem from './PostItem'
 import {useDispatch,useSelector}  from 'react-redux'
 
 const Posts = () => {
+
+    const dispatch =useDispatch()
+
+    const posts = useSelector((state)=>state.post.posts)
     return (
         <div>
             <button
                 type='submit'
                 className='bg-lime-300  hover:bg-lime-400 transition-all p-2 text-sm'
+               onClick={()=>dispatch(getPosts())}
             >
                 Get posts
             </button>
-            <PostItem />
+            {posts?.map((post)=>(
+            <PostItem 
+            post={post}
+            
+            key={post.title}/>
+
+            ))}
         </div>
     )
 }
