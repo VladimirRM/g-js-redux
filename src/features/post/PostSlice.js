d4a648592183846d6d5e553e111353e28797eb3d
Vladimir RM <shvdrummer@gmail.com>
2023-06-29 10:26:54 +0300

    Create postSlice

---
+++ src/features/post/PostSlice.js
@@ -0,0 +1,15 @@
+import   { createSlice}  from '@reduxjs/toolkit'
+
+const initialState = {
+  posts:[],
+}
+
+export const postSlice = createSlice({
+  name:'posts',
+  initialState,
+  reducers:{
+    setPosts:(state,action)=>{
+      state.posts = action.payload
+    }
+  }
+})
