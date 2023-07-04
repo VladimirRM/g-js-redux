e2857b1657f33c7df29e905a534ec683d05df18f
Vladimir RM <shvdrummer@gmail.com>
2023-07-04 09:17:47 +0300

    Import dispatch and create obj dispatch

--- src/features/post/Posts.js
+++ src/features/post/Posts.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import PostItem from './PostItem'
+import { useDispatch,useSelector } from 'react-redux'
 
 const Posts = () => {
+    const dispatch = useDispatch()
+
+    const posts = useSelector((state)=> state.post.posts)
     return (
         <div>
             <button
