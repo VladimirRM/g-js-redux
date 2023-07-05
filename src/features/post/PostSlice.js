75f0da9de62e4e5b66442534167313493deb26d9
Vladimir RM <shvdrummer@gmail.com>
2023-07-05 08:47:24 +0300

    Add reducers and first action

--- src/features/post/PostSlice.js
+++ src/features/post/PostSlice.js
@@ -1,31 +1,16 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
 
 
 const initialState = {
   posts:[],
 }
 
-
-export const getPosts = createAsyncThunk(
-  'posts/getPosts',async (_,{rejectWithValue,dispatch})=>{
-      const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
-      dispatch(res.data)
-  }
-)
-
-
-export const postSlice = createSlice({
-  name:'posts',
+export const  postSlice = createSlice({
+  name: 'posts',
   initialState,
   reducers: {
-    setPosts:(state,action)=>{
+    setPosts: (state,action)=>{
       state.posts = action.payload
     }
   }
-
 })
-
-export const {setPosts} = postSlice.actions
-export default postSlice.reducer
-
