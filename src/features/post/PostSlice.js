fcc6d86005f7252fa77d77a43ce42658de238272
Vladimir RM <shvdrummer@gmail.com>
2023-07-05 08:49:58 +0300

    Add axios method get

--- src/features/post/PostSlice.js
+++ src/features/post/PostSlice.js
@@ -1,9 +1,16 @@
 import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 
 const initialState = {
   posts:[],
 }
+
+export const getPosts = createAsyncThunk(
+  'posts/getPosts' , async (_,{rejectWithValue,dispatch})=>{
+      const res  = await axios.get('')
+  }
+)
 
 export const  postSlice = createSlice({
   name: 'posts',
