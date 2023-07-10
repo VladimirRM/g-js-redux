4582c8245000e9a76f8d8b29b160cb2265f75c74
Vladimir RM <shvdrummer@gmail.com>
2023-07-10 07:47:39 +0300

    Create postSlice and func reducers

--- src/features/post/PostSlice.js
+++ src/features/post/PostSlice.js
@@ -2,3 +2,16 @@ import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
 
 
 
+const initialState = { 
+  posts: [],
+}
+
+export const postSlice  = createSlice({
+  name:"posts",
+  initialState,
+  reducers:{
+    setPosts:(state,action)=>{
+      
+    }
+  }
+})
