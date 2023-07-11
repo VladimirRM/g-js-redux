b08c34a03e529c1ee50af6ac67f304310b96dbcd
Vladimir RM <shvdrummer@gmail.com>
2023-07-11 08:12:18 +0300

    Change code PostItem

--- src/features/post/PostItem.js
+++ src/features/post/PostItem.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
-import { deletePostById } from './PostSlice'
 
-const PostItem = ({post}) => {
-  const dispatch = useDispatch()
+const PostItem = () => {
     return (
-        <div className='flex w-full bg-indigo-500 hover:bg-indigo-300 transition-all py-1 px-2 text-white rounded-sm cursor-pointer mt-4'
-         onClick={()=> dispatch(deletePostById(post.id))}
-        >
-            {post.title}
+        <div className='flex w-full bg-indigo-500 hover:bg-indigo-300 transition-all py-1 px-2 text-white rounded-sm cursor-pointer mt-4'>
+            Post title
         </div>
     )
 }
