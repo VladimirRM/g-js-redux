70607a78a762f80b32ca50b3d0ba7872c23d45ec
Vladimir RM <shvdrummer@gmail.com>
2023-07-12 08:00:56 +0300

    Create first action and reducer

---
+++ src/features/post/PostSlice.js
@@ -0,0 +1,17 @@
+import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
+
+
+
+const initialState = {
+  posts:[],
+}
+
+export const postSlice = createSlice({
+  name:'posts',
+  initialState,
+  reducers:{
+    setPosts:(state,action)=>{
+      state.posts = action.payload
+    }
+  }
+})
