ff6d1ef69087937631e102064107cce7a5d44674
Vladimir RM <shvdrummer@gmail.com>
2023-07-13 07:45:31 +0300

    Add initialState.

---
+++ src/features/post/PostSlice.js
@@ -0,0 +1,15 @@
+import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
+
+
+
+const initialState = {
+  posts:[],
+}
+
+export const postSlice = createSlice({
+  name:'posts',
+  initialState,
+  reducers:{
+    setPosts:(state,action)
+  }
+})
