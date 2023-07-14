edae74a3834335c6ddde30887baa2e4896d451ed
Vladimir RM <shvdrummer@gmail.com>
2023-07-14 07:18:40 +0300

    update code Posts

--- src/features/post/Posts.js
+++ src/features/post/Posts.js
@@ -1,27 +1,16 @@
 import React from 'react'
 import PostItem from './PostItem'
-import {useSelector, useDispatch} from "react-redux"
-import { getPosts } from './PostSlice'
 
 const Posts = () => {
-    const dispatch = useDispatch()
-    const posts = useSelector((state)=> state.post.posts)
     return (
         <div>
             <button
                 type='submit'
                 className='bg-lime-300  hover:bg-lime-400 transition-all p-2 text-sm'
-              onClick={()=> dispatch(getPosts())}
             >
                 Get posts
             </button>
-            {posts.map(post=>(
-            <PostItem
-            key={post.title}
-            post={post}
-            />
-
-            ))}
+            <PostItem />
         </div>
     )
 }
