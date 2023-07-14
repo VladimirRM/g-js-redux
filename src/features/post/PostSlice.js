490e91af3a6731d4b4d5f33cb110a3e83da055a5
Vladimir RM <shvdrummer@gmail.com>
2023-07-14 07:34:13 +0300

    Create func deletePostById.

--- src/features/post/PostSlice.js
+++ src/features/post/PostSlice.js
@@ -7,6 +7,13 @@ const initialState = {
   posts: [],
 }
 
+export const deletePostById = createAsyncThunk(
+  'posts/deletePostById', async(id,{rejectWithValue,dispatch})=>{
+       await axios.delete(`https://jsonplaceholder.typicode.com/posts'/${id}`)
+       dispatch(deletePost(id))
+  }
+)
+
 export const getPosts = createAsyncThunk(
   'posts/getPosts', async (_,{rejectWithValue,dispatch})=>{
     const res  = await axios.get('https://jsonplaceholder.typicode.com/posts')
