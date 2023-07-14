ba2b54bef051a221125f74bb6f6f78f4ad0a1f91
Vladimir RM <shvdrummer@gmail.com>
2023-07-14 07:25:27 +0300

    Add axios for the request

--- src/features/post/PostSlice.js
+++ src/features/post/PostSlice.js
@@ -6,6 +6,12 @@ const initialState = {
   posts: [],
 }
 
+export const getPosts = createAsyncThunk(
+  'posts/getPosts', async (_,{rejectWithValue,dispatch})=>{
+    const res  = await axios.get()
+  }
+)
+
 export const postSlice = createSlice({
   name: 'posts',
   initialState,
