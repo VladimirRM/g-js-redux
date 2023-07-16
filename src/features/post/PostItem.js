684ac7c66ceb249cdaed71472a15f88c1f00d28a
Vladimir RM <shvdrummer@gmail.com>
2023-07-16 09:19:54 +0300

    Add func deletePostById.

--- src/features/post/PostItem.js
+++ src/features/post/PostItem.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const PostItem = ([post]) => {
+const PostItem = ({post}) => {
     return (
         <div className='flex w-full bg-indigo-500 hover:bg-indigo-300 transition-all py-1 px-2 text-white rounded-sm cursor-pointer mt-4'>
             {post.title}
--- src/features/post/PostSlice.js
+++ src/features/post/PostSlice.js
@@ -6,6 +6,16 @@ import axios from "axios";
 const initialState = {
   posts: [],
 }
+
+
+export const deletePostById = createAsyncThunk(
+  'posts/deletePostById', async (id,{rejectWithValue,dispatch})=>{
+    await axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    dispatch(deletePost(id))
+  }
+)
+
+
 export const getPosts = createAsyncThunk(
   'posts/getPosts',async (_,{rejectWithValue,dispatch})=>{
      const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
