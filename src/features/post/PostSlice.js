261a79c3a0ea904903e30ef953bb3209fa298568
Vladimir RM <shvdrummer@gmail.com>
2023-07-17 07:52:06 +0300

    Export first action and reducer.

--- src/features/post/PostSlice.js
+++ src/features/post/PostSlice.js
@@ -11,9 +11,10 @@ export const postSlice =  createSlice({
   initialState,
   reducers:{
       setPosts:(state,action)=>{
-
+      state.posts = action.payload
       }
   }
 })
-export cons 
+export  const {setPosts} = postSlice.actions
+export default postSlice.reducer
 
