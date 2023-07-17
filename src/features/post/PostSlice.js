818807067354ab63fe67cc1ef13e76dfa9c5c572
Vladimir RM <shvdrummer@gmail.com>
2023-07-17 07:49:52 +0300

    Add postSlice.

--- src/features/post/PostSlice.js
+++ src/features/post/PostSlice.js
@@ -6,4 +6,11 @@ import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
 const initialState = {
   posts:[],
 }
+export const postSlice =  createSlice({
+  name:'posts',
+  initialState,
+  reducers:{
+    
+  }
+})
 
