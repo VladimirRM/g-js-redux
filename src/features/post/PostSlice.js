3b718452847ba92bbd4703811569524461440dde
Vladimir RM <shvdrummer@gmail.com>
2023-07-18 08:29:46 +0300

    Create reducer amd first action.

--- src/features/post/PostSlice.js
+++ src/features/post/PostSlice.js
@@ -7,3 +7,14 @@ const initialState = {
 }
 
 
+export const postSlice = createSlice({
+  name:'posts',
+  initialState,
+  reducers:{
+    setPosts:( state,action)=>{
+      state.posts = action.payload
+    }
+  }
+})
+
+
