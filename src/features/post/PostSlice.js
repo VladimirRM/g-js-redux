114e66e4e6e8a52f2e7ad39944b09724a02a6e1e
Vladimir RM <shvdrummer@gmail.com>
2023-07-21 10:16:34 +0300

    Created func getPosts.

--- src/features/post/PostSlice.js
+++ src/features/post/PostSlice.js
@@ -1,10 +1,18 @@
 import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 
 
 const initialState = {
   posts: [],
 }
+
+export const getPosts = createAsyncThunk(
+  'posts/getPosts', async (_,{rejectWithValue,dispatch})=>{
+     const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+     dispatch(setPosts(res.data))
+  }
+)
 
 
 
