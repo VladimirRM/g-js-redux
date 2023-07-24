ef6f1d9eddd9bb6e95c550d6e1194f883d3cb3ff
Vladimir RM <shvdrummer@gmail.com>
2023-07-24 08:24:36 +0300

    Add dispatch in the Posts.

--- src/features/post/Posts.js
+++ src/features/post/Posts.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import PostItem from './PostItem'
+import { useDispatch,useSelector } from 'react-redux'
+import { getPosts } from './PostSlice'
 
 const Posts = () => {
+
+    const dispatch = useDispatch()
+    const posts = useSelector((state)=> state.post.posts)
     return (
         <div>
             <button
                 type='submit'
                 className='bg-lime-300  hover:bg-lime-400 transition-all p-2 text-sm'
+              onClick={dispatch(getPosts)}
             >
                 Get posts
             </button>
