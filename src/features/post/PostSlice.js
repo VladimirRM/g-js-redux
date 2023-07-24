98e3b30cf5cb65c0dc557106f173d7e0e1160b77
Vladimir RM <shvdrummer@gmail.com>
2023-07-24 08:31:27 +0300

    App work

--- src/features/post/PostSlice.js
+++ src/features/post/PostSlice.js
@@ -12,6 +12,13 @@ export const getPosts = createAsyncThunk(
   }
 )
 
+export const deletePostById = createAsyncThunk(
+  'posts/deletePostById', async (id,{rejectWithValue,dispatch})=>{
+       await axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
+       dispatch(deletePost(id))
+  }
+)
+
 
 export const postSlice = createSlice({
   name: 'posts',
@@ -19,10 +26,13 @@ export const postSlice = createSlice({
   reducers:{
     setPosts:(state,action)=>{
       state.posts =action.payload
+    },
+    deletePost: (state,action)=>{
+      state.posts = state.posts.filter(post=>post.id !== action.payload)
     }
   }
 })
-export const { setPosts} = postSlice.actions
+export const { setPosts,deletePost} = postSlice.actions
 export default postSlice.reducer
 
 
